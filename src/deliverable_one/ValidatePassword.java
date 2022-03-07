8888ac8f90dec1bc26b163e286a44add0a84188c
tristanbeatty95 <tristanbeatty@ymail.com>
2022-03-07 17:10:41 -0500

    First commit

---
+++ src/deliverable_one/ValidatePassword.java
@@ -0,0 +1,38 @@
+package deliverable_one;
+import java.util.Scanner;
+
+
+public class ValidatePassword {
+	
+	public static void main(String[] args) {
+		final int NUM_UP = 1;
+		final int NUM_LOW = 1;
+		final int NUM_NUM = 1;
+		
+		int hasNum = 0;
+		int hasLow = 0;
+		int hasUp = 0;
+		
+		
+		Scanner in = new Scanner(System.in);
+		System.out.println("Please enter your password: ");
+		String password = in.nextLine();
+		int passwordLength = password.length();
+		
+		for (int i = 0; i < passwordLength; i++) {
+			char c = password.charAt(i);
+			if (Character.isUpperCase(c))
+				hasUp++;
+			else if (Character.isLowerCase(c))
+				hasLow++;
+			else if (Character.isDigit(c))
+				hasNum++;		
+		}
+		
+		if(hasUp >= NUM_UP && hasLow >= NUM_LOW && hasNum >= NUM_NUM && password.length() >=7 && password.length() <= 12 && password.contains("!"))
+			System.out.println("Password valid and accepted");
+		else {
+			System.out.println("Error");
+		}
+	}
+}
